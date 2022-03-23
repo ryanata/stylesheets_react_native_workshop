7629af75247f00b0004bbb4e26a0c2b3d92d1fc9
Ryan Ata <44108244+ryanata@users.noreply.github.com>
2022-03-23 13:46:18 -0400

    Create App_style.js

---
+++ App_style.js
@@ -0,0 +1,42 @@
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#3E00FF',
+    flex: 1
+  },
+  title: {
+      textAlign: 'center',
+      fontSize: 30,
+      color: 'white',
+      fontWeight: '600',
+      marginTop: 20
+
+  },
+  titleBold: {
+    fontWeight: '900',
+    color: '#6D28D9',
+  },
+  btnNewTask: {
+    backgroundColor: '#6D28D9',
+    padding: 15,
+    marginTop: 30,
+    marginHorizontal: 20,
+    borderRadius: 10
+  },
+  btnTextNewTask: {
+    textAlign: 'center',
+    color: '#FFF',
+    fontSize: 18,
+    fontWeight: '900',
+    textTransform: 'uppercase'
+  },
+  notasks: {
+    marginTop: 40,
+    textAlign: 'center',
+    fontSize: 24,
+    fontWeight: '600'
+  },
+  list: {
+    marginTop: 50,
+    marginHorizontal: 30
+  }
+})
