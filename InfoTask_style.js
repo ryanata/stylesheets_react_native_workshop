df4b61b1d59cd431db54887180d794b724abfb95
Ryan Ata <44108244+ryanata@users.noreply.github.com>
2022-03-23 13:47:54 -0400

    Create InfoTask_style.js

---
+++ InfoTask_style.js
@@ -0,0 +1,58 @@
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#F59E0B',
+        flex: 1
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: '600',
+        textAlign: 'center',
+        marginTop: 30,
+        color: '#FFF'
+    },
+    titleBold: {
+        fontWeight: '900'
+    },
+    btnClose: {
+        marginVertical: 30,
+        backgroundColor: '#E06900',
+        marginHorizontal: 30,
+        padding: 15,
+        borderRadius: 10,
+    },
+    btnCloseText: {
+        color: '#FFF',
+        textAlign: 'center',
+        fontWeight: '900',
+        fontSize: 16,
+        textTransform: 'uppercase',
+    },
+    content: {
+        backgroundColor: '#FFF',
+        marginHorizontal: 30,
+        borderRadius: 10,
+        padding: 20,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    view: {
+        marginBottom: 10
+    },
+    label: {
+        textTransform: 'uppercase',
+        color: '#374151',
+        fontWeight: '600',
+        fontSize: 12
+    },
+    value: {
+        fontWeight: '700',
+        fontSize: 20,
+        color: '#334155'
+    }
+})
