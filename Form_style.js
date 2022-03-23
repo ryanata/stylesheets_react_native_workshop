c44dda0159acf587e0fa7600de83f0b3839f0d3d
Ryan Ata <44108244+ryanata@users.noreply.github.com>
2022-03-23 13:47:25 -0400

    Create Form_style.js

---
+++ Form_style.js
@@ -0,0 +1,67 @@
+const styles = StyleSheet.create({
+    content: {
+        backgroundColor: '#6D28D9',
+        flex: 1,
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: '600',
+        textAlign: 'center',
+        marginTop: 30,
+        color: '#FFF'
+    },
+    titleBold: {
+        fontWeight: '900'
+    },
+    btnCancel: {
+        marginVertical: 30,
+        backgroundColor: '#5827A4',
+        marginHorizontal: 30,
+        padding: 15,
+        borderRadius: 10,
+    },
+    btnCancelText: {
+        color: '#FFF',
+        textAlign: 'center',
+        fontWeight: '900',
+        fontSize: 16,
+        textTransform: 'uppercase',
+    },
+    view: {
+        marginTop: 10,
+        marginHorizontal: 30,
+    },
+    label: {
+        color: '#FFF',  
+        marginBottom: 10,
+        marginTop: 15,
+        fontSize: 20,
+        fontWeight: '600'
+    },
+    input: {
+        backgroundColor: '#FFF',
+        padding: 15,
+        borderRadius: 10
+    },
+    course: {
+        height: 100
+    },
+    dateContainer: {
+        backgroundColor: '#FFF',
+        borderRadius: 10
+    },
+    btnNewTask: {
+        marginVertical: 50,
+        backgroundColor: '#F59E0B',
+        paddingVertical: 15,
+        marginHorizontal: 30,
+        borderRadius: 10
+    },
+    btnNewTaskText: {
+        color: '#5827A4',
+        textAlign: 'center',
+        fontWeight: '900',
+        fontSize: 16,
+        textTransform: 'uppercase',
+    }
+})
