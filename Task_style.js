a64ebc054a362c03b11273d754d1770e6daa53dd
Ryan Ata <44108244+ryanata@users.noreply.github.com>
2022-03-23 13:48:26 -0400

    Create Task_style.js

---
+++ Task_style.js
@@ -0,0 +1,46 @@
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#FFF',
+        padding: 20,
+        borderBottomColor: '#94a3B8',
+        borderBottomWidth: 1
+    },
+    label: {
+        color: '#374151',
+        textTransform: 'uppercase',
+        fontWeight: '700',
+        marginBottom: 10
+    },
+    text: {
+        color: '#6D28D9',
+        fontSize: 24,
+        fontWeight: '700',
+        marginBottom: 10
+    },
+    deadline: {
+        color: '#374151'
+    },
+    containerButtons: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 20
+    },
+    btn: {
+        paddingVertical: 5,
+        paddingHorizontal: 20,
+        borderRadius: 5
+    },
+    btnEditar: {
+        backgroundColor: '#F59E0B'
+    },
+    btnDelete: {
+        backgroundColor: '#EF4444'
+    },
+    btnText: {
+        textTransform: 'uppercase',
+        fontWeight: '700',
+        fontSize: 12,
+        color: '#FFF'
+    }
+
+})
